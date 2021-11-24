979136ddb21ef78587e2f904ded11d09e7a32cad
Alex Birchi <birchialex@gmail.com>
2021-11-24 17:36:01 +0200

    grammar

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import ply.lex as lex
+import ply.yacc as yacc
+
 from cminlex import *
+from cmingrammar import *
+
 
 lexer = lex.lex()
 with open('example.c' , 'r') as example_file:
@@ -7,7 +11,11 @@ with open('example.c' , 'r') as example_file:
 
 lexer.input(data)
 
-while 1:
-    token = lexer.token()
-    if not token: break
-    print('{}\t\t\t{}\t\t\t{}\t\t\t{}'.format(token.type, token.value, token.lineno, token.lexpos))
+# while 1:
+#     token = lexer.token()
+#     if not token: break
+#     print('{}\t\t\t{}\t\t\t{}\t\t\t{}'.format(token.type, token.value, token.lineno, token.lexpos))
+
+
+parser = yacc.yacc()
+print(parser.parse(data))
