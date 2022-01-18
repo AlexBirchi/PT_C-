6ec1f3593a4d6d4e6167b1d6b8089f5bfcaa3cf7
Alex Birchi <birchialex@gmail.com>
2022-01-18 22:29:48 +0200

    AWS-tree

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ import ply.yacc as yacc
 from cminlex import *
 from cmingrammar import *
 
+from utils import CustomEncoder
 
-lexer = lex.lex()
 with open('example.c' , 'r') as example_file:
     data = example_file.read()
 
+lexer = lex.lex()
 lexer.input(data)
 
-# while 1:
-#     token = lexer.token()
-#     if not token: break
-#     print('{}\t\t\t{}\t\t\t{}\t\t\t{}'.format(token.type, token.value, token.lineno, token.lexpos))
-
-
 parser = yacc.yacc()
-print(parser.parse(data))
+program = parser.parse(data)
+
+print(CustomEncoder().encode(program))
